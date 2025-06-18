fe06fa26f37514f8546eb9beb58fa92da69b802c
ArthurLaender <135544713+ArthurLaender@users.noreply.github.com>
2025-06-18 19:40:30 -0300

    Update ETL2.py

--- ETL2.py
+++ ETL2.py
@@ -322,12 +322,12 @@ with col_centro:
 
     # Adiciona customdata com a contagem e ajusta o hovertemplate
     fig_heat.update_traces(
-        customdata=count_data.values.astype(float),
+        customdata=np.expand_dims(count_data.values, axis=-1),
         hovertemplate=
         "Ano: %{y}<br>" +
         "Renda: %{x}<br>" +
         "Média de Notas: %{z:.2f}<br>" +
-        "Participantes: %{customdata:.0f}<extra></extra>"
+        "Participantes: %{customdata[0]:.0f}<extra></extra>"
     )
 
     # Ajusta os eixos e layout
